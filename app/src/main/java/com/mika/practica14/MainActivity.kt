fbc375355e874260c3ca78a935ddd1b9338ea449
Marco Antonio Aguayo <MikoGrenore@Outlook.com>
2021-10-04 03:10:28 -0500

    Initial commit

---
+++ app/src/main/java/com/mika/practica14/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mika.practica14
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
